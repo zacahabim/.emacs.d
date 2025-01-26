b32c6c908b2791146237b2467dff5bd1134e6cd9
Hieu Nguyen <zacahabim@gmail.com>
2025-01-26 02:16:57 +0200

    Change to Jetbrains font. This looks somewhat better

--- init.el
+++ init.el
@@ -199,14 +199,14 @@
 
 (cond ((my-system-type-is-darwin)
 (custom-set-faces
- '(default ((t (:family "GoMono Nerd Font Mono" :foundry "nil" :slant normal :weight regular :height 150 :width normal)))))
+ '(default ((t (:family "JetBrainsMono Nerd Font" :foundry "JB" :slant normal :weight regular :height 150 :width normal)))))
 ) (t
     (custom-set-faces
-      '(default ((t (:family "GoMono Nerd Font Mono" :foundry "nil" :slant normal :weight regular :height 120 :width normal)))))
+      '(default ((t (:family "JetBrainsMono Nerd Font" :foundry "JB" :slant normal :weight regular :height 120 :width normal)))))
 ))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "GoMono Nerd Font Mono" :foundry "nil" :slant normal :weight regular :height 120 :width normal)))))
+ '(default ((t (:family "JetBrainsMono Nerd Font" :foundry "JB" :slant normal :weight regular :height 120 :width normal)))))
